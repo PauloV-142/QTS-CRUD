1bb14672c7e722074147ca3a59580166afe8aabb
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2026-06-15 22:13:54 -0300

    Basic insert working

--- inserir.php
+++ inserir.php
@@ -9,39 +9,67 @@
     <body class="bg-dark text-light">
 
         <?php
+            if (isset($_POST['nome'])) {
+            # Form preenchido.
+
+            $nome = $_POST["nome"];
+            $data_nascimento = $_POST["data_nascimento"];
+            $cpf = $_POST["cpf"];
+            $matricula = $_POST["matricula"];
+            $instituicao = $_POST["instituicao"];
+            $serie = $_POST["serie"];
+            $curso = $_POST["curso"];
+            
+
             include_once("conexao.php");
+
+            $sql_insert = "INSERT INTO alunos (nome, data_nacimento, cpf, matricula, instituicao, serie, curso) VALUES ('$nome', '$data_nascimento', '$cpf', $matricula, '$instituicao', '$serie', '$curso')";
+
+            $resultado = @mysqli_query($conexao, $sql_insert);
+            echo "$resultado";
+
+            $mysqli->close();
+            }
         ?>
 
         <?php 
-        include_once("links.php");
+        include_once("components/links.php");
         echo $links;
         ?>
         <main class="center">
 
             <form action="inserir.php" method="post">
                 <h2>Registrar Aluno</h2>
+                
                 <div class="container-round bg-darken text-light" id="consultaDiv">
-                <label for="nome">Nome:</label>
-                <input name="nome"></input>
                 
-                <label for="data_nascimento" >Data de Nascimento</label>
-                <input name="data_nascimento"></input>
-
-                <label for="cpf" >CPF</label>
-                <input name="cpf"></input>
-
-                <label for="matricula">Matricula</label>
-                <input name="matricula"></input>
-                
-                <label for="instituicao" >Instituição</label>
-                <input name="instituicao"></input>
-                
-                <label for="serie" >Serie</label>
-                <input name="serie"></input>
-                
-                <label for="curso" >Curso</label>
-                <input name="curso"></input>
+                <div>
+                    <label for="nome">Nome:</label>
+                    <input name="nome"></input>
+                    
+                    <label for="data_nascimento" >Data de Nascimento</label>
+                    <input name="data_nascimento"></input>
+                    
+                    <label for="cpf" >CPF</label>
+                    <input name="cpf"></input>
                 </div>
+
+                <div>
+                    <label for="matricula">Matricula</label>
+                    <input name="matricula"></input>
+                    
+                    <label for="instituicao" >Instituição</label>
+                    <input name="instituicao"></input>
+                    
+                    <label for="serie" >Serie</label>
+                    <input name="serie"></input>
+                    
+                    <label for="curso" >Curso</label>
+                    <input name="curso"></input>
+                    
+                    <input type="submit" value="Enviar"></input>
+                </div>
+            </div>
             </form>
         </main>
     </body>
