e9b688ecbc5f5a557bf4af2843fd1d847f7e707c
Joao-Pedro-123 <joao.almeida332@etec.sp.gov.br>
2026-06-24 19:14:00 -0300

    atualização dos links header

--- components/header.php
+++ components/header.php
@@ -1,13 +1,13 @@
 <?php
 $windows_path = "C:/wamp64/www/QTSCRUD";
-$jp_path = "xampp\htdocs\qts";
+$jp_path = "/qts";
 $linux_path = "/QTSCRUD";
 ?>
 <header class="text-light"> <h1> Atividade QTS operações CRUD </h1> </header> 
     <navbar>    
-    <a href='#'> Início </a> 
-    <a href='pages/indexconsultar.php'> Consultar </a> 
-    <a href='pages/indexinserir.php'> Inserir </a> 
-    <a href='pages/indexatualizar.php'> Atualizar </a> 
-    <a href='pages/indexremover.php'> Remover </a> 
+    <a href='<?= $jp_path ?>/index.php'> Início </a> 
+    <a href='<?= $jp_path ?>/pages/indexconsultar.php'> Consultar </a> 
+    <a href='<?= $jp_path ?>/pages/indexinserir.php'> Inserir </a> 
+    <a href='<?= $jp_path ?>/pages/indexatualizar.php'> Atualizar </a> 
+    <a href='<?= $jp_path ?>/pages/indexremover.php'> Remover </a> 
 </navbar>
